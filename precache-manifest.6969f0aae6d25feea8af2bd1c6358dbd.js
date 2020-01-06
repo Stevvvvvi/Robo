eb89a4a5b099e36815659d670e1267fa433de62f
Stevvvvvi <lly363893886@gmail.com>
2020-01-07 07:12:51 +1100

    Updates

---
+++ precache-manifest.6969f0aae6d25feea8af2bd1c6358dbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dfc7cb9dccec8c4139f030d5192bf3",
+    "url": "/Robo/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/Robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0de7704545ffb7a088cd",
+    "url": "/Robo/static/css/main.560204b5.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/Robo/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robo/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0de7704545ffb7a088cd",
+    "url": "/Robo/static/js/main.9174e832.chunk.js"
+  },
+  {
+    "revision": "6e61518011c5cf91f2e6",
+    "url": "/Robo/static/js/runtime-main.91e00228.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
